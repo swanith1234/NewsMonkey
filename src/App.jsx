557871379024ef443deee3b0b3ff08c3da3bd3b5
swanith1234 <swanithpidugu@gmail.com>
2024-07-09 17:36:58 +0530

    added basename in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar';
-import Newscomponent from './Newscomponent';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Newscomponent from "./Newscomponent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
-
-
   render() {
     return (
-      <Router>
+      <Router basename="/NewsMonkey/">
         <div>
           <Navbar />
           <div className="text-center">
@@ -16,15 +14,45 @@ export default class App extends Component {
             <h3>Top Headlines</h3>
           </div>
           <Routes>
-          <Route path="/" element={<Newscomponent key={"general"  }category="general"/>} />
-          <Route path="/entertainment" element={<Newscomponent key="entertainment" category="entertainment"/>} />
-          <Route path="/sports" element={<Newscomponent key="sports" category="sports"/>} />
-          <Route path="/business" element={<Newscomponent  key="business" category="business"/>} />
-          <Route path="/politics" element={<Newscomponent key="politics" category="politics"/>} />
-          <Route path="/education" element={<Newscomponent  key="education" category="science"/>} />
-          <Route path="/health" element={<Newscomponent  key="health" category="health"/>} />
-          <Route path="/technology" element={<Newscomponent  key="technology" category="technology"/>} />
-          <Route path="/travel" element={<Newscomponent key="travel" category="travel"/>} /></Routes>
+            <Route
+              path="/"
+              element={<Newscomponent key={"general"} category="general" />}
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <Newscomponent key="entertainment" category="entertainment" />
+              }
+            />
+            <Route
+              path="/sports"
+              element={<Newscomponent key="sports" category="sports" />}
+            />
+            <Route
+              path="/business"
+              element={<Newscomponent key="business" category="business" />}
+            />
+            <Route
+              path="/politics"
+              element={<Newscomponent key="politics" category="politics" />}
+            />
+            <Route
+              path="/education"
+              element={<Newscomponent key="education" category="science" />}
+            />
+            <Route
+              path="/health"
+              element={<Newscomponent key="health" category="health" />}
+            />
+            <Route
+              path="/technology"
+              element={<Newscomponent key="technology" category="technology" />}
+            />
+            <Route
+              path="/travel"
+              element={<Newscomponent key="travel" category="travel" />}
+            />
+          </Routes>
         </div>
       </Router>
     );
